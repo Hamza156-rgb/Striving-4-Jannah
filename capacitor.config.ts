5014d479ff4e9633cc81bc0442943fca2214ea00
Hamza Waseem <hamza@techbanq.net>
2026-05-09 16:49:13 +0500

    code updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.noor.islamicapp',
-  appName: 'Noor - نور',
+  appId: 'com.striving4jannah.app',
+  appName: 'Striving 4 Jannah',
   webDir: 'dist/noor-app/browser',
   server: {
     androidScheme: 'https'
