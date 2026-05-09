69e596d59d42802f8eb9b782e65dc015b3dc2c2f
Hamza Waseem <hamza@techbanq.net>
2026-05-09 19:41:33 +0500

    code updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Capacitor } from '@capacitor/core';
+import { filter } from 'rxjs/operators';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
@@ -11,4 +15,21 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   standalone: true,
   imports: [IonApp, IonRouterOutlet]
 })
-export class App {}
+export class App {
+  private router = inject(Router);
+  private title = inject(Title);
+
+  constructor() {
+    if (!Capacitor.isNativePlatform()) {
+      return;
+    }
+    const stripNativeCaption = () => {
+      this.title.setTitle('');
+      document.title = '';
+    };
+    stripNativeCaption();
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(stripNativeCaption);
+  }
+}
